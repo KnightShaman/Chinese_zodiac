323ed9c37d6a5dbfe969867961118d6a0eedbac9
KnightShaman <40634751+KnightShaman@users.noreply.github.com>
2019-03-14 20:52:59 +0200

    Add files via upload

---
+++ Chinese zodiac/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Chinese_zodiac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(231, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Find zodiac";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(288, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 390);
+            this.label3.TabIndex = 4;
+            this.label3.Text = resources.GetString("label3.Text");
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(464, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 390);
+            this.label4.TabIndex = 5;
+            this.label4.Text = resources.GetString("label4.Text");
+            this.label4.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(167, 51);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(47, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "A.D.";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(115, 51);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "B.C.";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            this.checkBox2.Click += new System.EventHandler(this.checkBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "type year above";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(133, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "select era";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.Form1_Load);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 208);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = " Chinese zodiac calendar converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Chinese zodiac/Form1.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chinese_zodiac
+{
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+           // button1.Enabled = false;
+            textBox1.MaxLength = 5;
+            textBox1.Focus();
+    }
+
+        private void button1_Click(object sender, EventArgs e) //calculation functional
+        {
+            int year = Convert.ToInt32(textBox1.Text);
+            int i ;
+            int reminder;
+            if (checkBox2.Checked == true)
+            {
+                i = ((Math.Abs(year) + 2) / 60 );
+                reminder = ((Math.Abs(year)+ 2) - (60 * i));
+                i = 60 - reminder;
+
+
+                /* uncomment to test result 
+                string Text = reminder.ToString();
+                MessageBox.Show(Text); 
+                */
+
+                label2.Visible = true;
+                RsultToLabel(reminder);
+            }
+            else if (checkBox1.Checked == true)
+            {
+                i = (year - 3) / 60;
+                reminder = (year - 3) - (60 * i);
+
+                /* uncomment to test result 
+                string Text = reminder.ToString();
+                MessageBox.Show(Text); 
+                */   
+                
+                label2.Visible = true;
+                RsultToLabel(reminder);
+
+            }
+
+
+
+
+        } 
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // 1-4 is a bug 
+
+            label2.Visible = false; // hides previous result
+
+            if ( textBox1.Text.Length > 0 ) 
+                
+            {
+                int input = Convert.ToInt32(textBox1.Text);
+                if (Convert.ToInt32(textBox1.Text) == 0)
+                {
+                    checkBox1.Checked = false;
+                    checkBox2.Checked = false;
+                    checkBox1.Enabled = false;
+                    checkBox2.Enabled = false;
+                    label1.Visible = true;
+                    label1.Text = "there is no zero year";
+                }
+                else
+                {
+                    label1.Text = "type year above";
+                    label1.Visible = false;
+                    checkBox1.Enabled = true;
+                    checkBox2.Enabled = true;
+                }
+                switch (input)
+                {
+                    case 1:
+                        label2.Text = "辛 Yin Metal 酉 Rooster";
+                        break;
+                    case 2:
+                        label2.Text = "壬 Yang Water 戌 Dog";
+                        break;
+                    case 3:
+                        label2.Text = "癸 Yin Water 亥 Pig";
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+                    
+                if ((checkBox2.Checked == true) && (checkBox1.Checked == true))
+                {
+                    button1.Enabled = false;
+                }
+                else if ((checkBox2.Checked == false) && (checkBox1.Checked == false))
+                {
+                    button1.Enabled = false;
+                }
+                else
+                {
+                    button1.Enabled = true;
+                }
+            }
+
+            else
+            {
+                button1.Enabled = false;
+                label1.Visible = true;
+            }
+            
+        }
+             
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            
+            if ((textBox1.Text.Length > 0) && (checkBox2.Checked == true) && (checkBox1.Checked == false))
+            {
+                button1.Enabled = true;
+                label5.Visible = false;
+            }
+            else if ((textBox1.Text.Length > 0) && (checkBox1.Checked == true) && (checkBox2.Checked == false))
+            {
+                button1.Enabled = true;
+                label5.Visible = false;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+
+            if ((checkBox1.Checked) || (checkBox2.Checked)) //hide select era text
+            {
+                label5.Visible = false;
+            }
+            else
+            {
+                label5.Visible = true;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if ((textBox1.Text.Length > 0) && (checkBox1.Checked == true) && (checkBox2.Checked == false))
+            {
+                button1.Enabled = true;
+                
+            }
+            else if ((textBox1.Text.Length > 0) && (checkBox2.Checked == true) && (checkBox1.Checked == false))
+            {
+                button1.Enabled = true;
+                
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+
+            if ((checkBox1.Checked) || (checkBox2.Checked)) //hide select era text
+            {
+                label5.Visible = false;
+            }
+            else
+            {
+                label5.Visible = true;
+            }
+        }
+
+        private void checkBox2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = true;
+            }
+        } // allow only 1 checked box at a time
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                checkBox2.Checked = false;
+                checkBox1.Checked = true;
+            }
+        } // allow only 1 checked box at a time
+
+        private void RsultToLabel(int reminder)
+        {
+            switch (reminder)
+            {
+                case 1:
+                    label2.Text = "甲 Yang Wood	子 Rat";
+                    break;
+                case 2:
+                    label2.Text = "乙 Yin Wood 丑 Ox";
+                    break;
+                case 3:
+                    label2.Text = "丙 Yang Fire 寅 Tiger";
+                    break;
+                case 4:
+                    label2.Text = "丁 Yin Fire 卯 Rabbit";
+                    break;
+                case 5:
+                    label2.Text = "戊 Yang Earth 辰 Dragon";
+                    break;
+                case 6:
+                    label2.Text = "己 Yin Earth	巳 Snake";
+                    break;
+                case 7:
+                    label2.Text = "庚 Yang Metal 午 Horse";
+                    break;
+                case 8:
+                    label2.Text = "辛 Yin Metal 未 Goat";
+                    break;
+                case 9:
+                    label2.Text = "壬 Yang Water 申 Monkey";
+                    break;
+                case 10:
+                    label2.Text = "癸 Yin Water 酉 Rooster";
+                    break;
+
+                case 11:
+                    label2.Text = "甲 Yang Wood 戌 Dog";
+                    break;
+                case 12:
+                    label2.Text = "乙 Yin Wood 亥 Pig";
+                    break;
+                case 13:
+                    label2.Text = "丙 Yang Fire 子 Rat";
+                    break;
+                case 14:
+                    label2.Text = "丁 Yin Fire 丑 Ox";
+                    break;
+                case 15:
+                    label2.Text = "戊 Yang Earth 寅 Tiger";
+                    break;
+                case 16:
+                    label2.Text = "己 Yin Earth 卯 Rabbit";
+                    break;
+                case 17:
+                    label2.Text = "庚 Yang Metal 辰 Dragon";
+                    break;
+                case 18:
+                    label2.Text = "辛 Yin Metal 巳 Snake";
+                    break;
+                case 19:
+                    label2.Text = "壬 Yang Water 午 Horse";
+                    break;
+                case 20:
+                    label2.Text = "癸 Yin Water 未 Goat";
+                    break;
+
+                case 21:
+                    label2.Text = "甲 Yang Wood 申 Monkey";
+                    break;
+                case 22:
+                    label2.Text = "乙 Yin Wood 酉 Rooster";
+                    break;
+                case 23:
+                    label2.Text = "丙 Yang Fire 戌 Dog";
+                    break;
+                case 24:
+                    label2.Text = "丁 Yin Fire 亥 Pig";
+                    break;
+                case 25:
+                    label2.Text = "戊 Yang Earth 子 Rat";
+                    break;
+                case 26:
+                    label2.Text = "己 Yin Earth 丑 Ox";
+                    break;
+                case 27:
+                    label2.Text = "庚 Yang Metal 寅 Tiger";
+                    break;
+                case 28:
+                    label2.Text = "辛 Yin Metal 卯 Rabbit";
+                    break;
+                case 29:
+                    label2.Text = "壬 Yang Water 辰 Dragon";
+                    break;
+                case 30:
+                    label2.Text = "癸 Yin Water 巳 Snake";
+                    break;
+
+                case 31:
+                    label2.Text = "甲 Yang Wood 午 Horse";
+                    break;
+                case 32:
+                    label2.Text = "乙 Yin Wood 未 Goat";
+                    break;
+                case 33:
+                    label2.Text = "丙 Yang Fire 申 Monkey";
+                    break;
+                case 34:
+                    label2.Text = "丁 Yin Fire 酉 Rooster";
+                    break;
+                case 35:
+                    label2.Text = "戊 Yang Earth 戌 Dog";
+                    break;
+                case 36:
+                    label2.Text = "己 Yin Earth 亥 Pig";
+                    break;
+                case 37:
+                    label2.Text = "庚 Yang Metal 子 Rat";
+                    break;
+                case 38:
+                    label2.Text = "辛 Yin Metal 丑 Ox";
+                    break;
+                case 39:
+                    label2.Text = "壬 Yang Water 寅 Tiger";
+                    break;
+                case 40:
+                    label2.Text = "癸 Yin Water 卯 Rabbit";
+                    break;
+
+                case 41:
+                    label2.Text = "甲 Yang Wood 辰 Dragon";
+                    break;
+                case 42:
+                    label2.Text = "乙 Yin Wood 巳 Snake";
+                    break;
+                case 43:
+                    label2.Text = "丙 Yang Fire 午 Horse";
+                    break;
+                case 44:
+                    label2.Text = "丁 Yin Fire 未 Goat";
+                    break;
+                case 45:
+                    label2.Text = "戊 Yang Earth 申 Monkey";
+                    break;
+                case 46:
+                    label2.Text = "己 Yin Earth 酉 Rooster";
+                    break;
+                case 47:
+                    label2.Text = "庚 Yang Metal 戌 Dog";
+                    break;
+                case 48:
+                    label2.Text = "辛 Yin Metal 亥 Pig";
+                    break;
+                case 49:
+                    label2.Text = "壬 Yang Water	子 Rat";
+                    break;
+                case 50:
+                    label2.Text = "癸 Yin Water 丑 Ox";
+                    break;
+
+                case 51:
+                    label2.Text = "甲 Yang Wood 寅 Tiger";
+                    break;
+                case 52:
+                    label2.Text = "乙 Yin Wood 卯 Rabbit";
+                    break;
+                case 53:
+                    label2.Text = "丙 Yang Fire 辰 Dragon";
+                    break;
+                case 54:
+                    label2.Text = "丁 Yin Fire 巳 Snake";
+                    break;
+                case 55:
+                    label2.Text = "戊 Yang Earth 午 Horse";
+                    break;
+                case 56:
+                    label2.Text = "己 Yin Earth 未 Goat";
+                    break;
+                case 57:
+                    label2.Text = "庚 Yang Metal 申 Monkey";
+                    break;
+                case 58:
+                    label2.Text = "辛 Yin Metal 酉 Rooster";
+                    break;
+                case 59:
+                    label2.Text = "壬 Yang Water 戌 Dog";
+                    break;
+                case 60:
+                    label2.Text = "癸 Yin Water 亥 Pig";
+                    break;
+                default: 
+                    break;
+            }
+
+        } // results list to display
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar) == 13)
+            {
+                button1.PerformClick(); // execute button1 code on Enter key pressed in textbox
+
+            }
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) //allow only digits
+            {
+                e.Handled = true;
+            }
+        }
+
+        
+    }
+}
